0e119eb6ba2f913458caef0437ae0bdda0001855
Luismi Ramirez <luismir89@gmail.com>
2014-02-01 20:16:39 +0100

    index, sections and pretty random words generator for data

--- app/controllers/alphabet_controller.rb
+++ app/controllers/alphabet_controller.rb
@@ -7,14 +7,22 @@ class AlphabetController < UIViewController
     self.view.addSubview(@table)
 
     @table.dataSource = self
-    @data = ("A".."Z").to_a
-
-    @table.dataSource = self
     @table.delegate = self
+
+    @data = {}
+    ("A".."Z").to_a.each do |letter|
+      @data[letter] = []
+      5.times do
+        # Via http://stackoverflow.com/a/88341/910451
+        # <3
+        random_string = (0..4).map{65.+(rand(25)).chr}.join
+        @data[letter] << letter + random_string
+      end  
+    end 
   end
 
   def tableView(tableView, numberOfRowsInSection: section)
-    @data.count
+    rows_for_section(section).count
   end
 
   def tableView(tableView, cellForRowAtIndexPath: indexPath)
@@ -25,24 +33,52 @@ class AlphabetController < UIViewController
             UITableViewCellStyleDefault,
             reuseIdentifier: @reuseIdentifier)
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator
-    cell.textLabel.text = @data[indexPath.row]
+    cell.textLabel.text = row_for_index_path(indexPath)
     cell
   end
 
   def tableView(tableView, didSelectRowAtIndexPath: indexPath)
     tableView.deselectRowAtIndexPath(indexPath, animated: true)
-    letter = @data[indexPath.row]
+    letter = sections[indexPath.section]
 
     controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
     controller.view.backgroundColor = UIColor.whiteColor
-    controller.title = letter
+    controller.title = "#{letter} - #{row_for_index_path(indexPath)}"
 
     label = UILabel.alloc.initWithFrame(CGRectZero)
-    label.text = letter
+    label.text = row_for_index_path(indexPath)
     label.sizeToFit
     label.center = [controller.view.frame.size.width / 2,
       controller.view.frame.size.height / 2]
     controller.view.addSubview(label)
     self.navigationController.pushViewController(controller, animated: true)
   end
+
+  def tableView(tableView, titleForHeaderInSection: section)
+    sections[section]
+  end
+
+  def tableView(tableView, sectionForSectionIndexTitle: title, atIndex: index)
+    sections.index title
+  end    
+
+  def sectionIndexTitlesForTableView(tableView) 
+    sections
+  end
+
+  def numberOfSectionsInTableView(tableView)
+    self.sections.count
+  end 
+
+  def sections
+    @data.keys.sort
+  end  
+
+  def rows_for_section(section_index)
+    @data[self.sections[section_index]]
+  end    
+
+  def row_for_index_path(index_path)
+    rows_for_section(index_path.section)[index_path.row]
+  end  
 end
