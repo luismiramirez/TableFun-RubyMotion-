65aa6203ed5ff19e67db1d0b88c792fa9b492920
Luismi Ramirez <luismir89@gmail.com>
2014-02-01 19:11:00 +0100

    tableViewMethods on controller to show full alphabet

---
+++ app/controllers/alphabet_controller.rb
@@ -0,0 +1,48 @@
+class AlphabetController < UIViewController
+  def viewDidLoad
+    super
+    self.title = "Alphabet"
+    @table = UITableView.alloc.initWithFrame(self.view.bounds)
+    @table.autoresizingMask = UIViewAutoresizingFlexibleHeight
+    self.view.addSubview(@table)
+
+    @table.dataSource = self
+    @data = ("A".."Z").to_a
+
+    @table.dataSource = self
+    @table.delegate = self
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    @data.count
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) 
+    cell = UITableViewCell.alloc.initWithStyle(
+            UITableViewCellStyleDefault,
+            reuseIdentifier: @reuseIdentifier)
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator
+    cell.textLabel.text = @data[indexPath.row]
+    cell
+  end
+
+  def tableView(tableView, didSelectRowAtIndexPath: indexPath)
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    letter = @data[indexPath.row]
+
+    controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
+    controller.view.backgroundColor = UIColor.whiteColor
+    controller.title = letter
+
+    label = UILabel.alloc.initWithFrame(CGRectZero)
+    label.text = letter
+    label.sizeToFit
+    label.center = [controller.view.frame.size.width / 2,
+      controller.view.frame.size.height / 2]
+    controller.view.addSubview(label)
+    self.navigationController.pushViewController(controller, animated: true)
+  end
+end
